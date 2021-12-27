8b6f9e8d692f6df5c99f3765c5aa0279b6c2b4d8
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2021-12-27 18:05:31 +0100

    Modified Inputs and Added Any to prevent strick template chekcing

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,30 @@
 import {NgModule} from "@angular/core";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FlexModule} from "@angular/flex-layout";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule {

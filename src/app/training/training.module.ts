7ee7c0248645ec8f76926a15f188a2eb291eee93
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-03 00:28:33 +0100

    training.module.ts imports

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -6,6 +6,10 @@ import {NewTrainingComponent} from "./new-training/new-training.component";
 import {PastTrainingsComponent} from "./past-trainings/past-trainings.component";
 import {RouterModule} from "@angular/router";
 import {StopTrainingComponent} from "./current-training/stop-training/stop-training.component";
+import {StoreModule} from "@ngrx/store";
+import * as TrainingFeature from './store/training.reducer';
+import {EffectsModule} from "@ngrx/effects";
+import {TrainingEffects} from "./store/training.effects";
 
 @NgModule({
   declarations: [
@@ -16,9 +20,10 @@ import {StopTrainingComponent} from "./current-training/stop-training/stop-train
     StopTrainingComponent
   ],
   imports: [SharedModule,
-    RouterModule.forChild([
-      {path: '', component: TrainingComponent}
-    ])],
+    RouterModule.forChild([{path: '', component: TrainingComponent}]),
+    StoreModule.forFeature(TrainingFeature.trainingFeature),
+    EffectsModule.forFeature([TrainingEffects])
+  ],
   exports: [RouterModule],
   providers: [],
   entryComponents: [StopTrainingComponent]

209866305673833353b8604eaf59817def158c2c
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-02 23:44:09 +0100

    fetch Available exercise. Untested

--- src/app/training/store/training.effects.ts
+++ src/app/training/store/training.effects.ts
@@ -1,8 +1,51 @@
 import {Injectable} from "@angular/core";
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/compat/firestore";
+import {Store} from "@ngrx/store";
+import {Exercise} from "../../exercise.model";
+import * as TrainingSelector from './training.selector';
+import * as TrainingActions from './training.actions';
+import {Actions, createEffect, ofType} from "@ngrx/effects";
+import {map, switchMap} from "rxjs";
 
 @Injectable()
-export class TrainingEffects{
-  constructor() {
+export class TrainingEffects {
+
+
+  fetchAvailabeExercises = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TrainingActions.FETCH_AVAL_EXERCISES),
+
+  // switch or exhaustMap
+      switchMap(() => {
+        return this.availableCollection.snapshotChanges().pipe(
+          map(documentArray => {
+            return documentArray.map(doc => {
+              return {
+                id: doc.payload.doc.id,
+                name: doc.payload.doc.data().name,
+                duration: +doc.payload.doc.data().duration,
+                calories: +doc.payload.doc.data().calories,
+                imgPath: doc.payload.doc.data().imgPath
+              };
+            });
+
+          }));
+      }),
+      map(value => {
+        return TrainingActions.SET_AVAL_EXERCISES({payload: value})
+      })
+    );
+  }, {dispatch: true});
+
+  private finishedCollection: AngularFirestoreCollection<Exercise>;
+  private availableCollection: AngularFirestoreCollection<Exercise>;
+
+  constructor(private fireDb: AngularFirestore,
+              private store: Store,
+              private actions$: Actions) {
+    this.availableCollection = this.fireDb.collection<Exercise>('availableExercises');
+    this.finishedCollection = this.fireDb.collection<Exercise>('finishedExercises');
+    this.store.select(TrainingSelector.selectTrainingViewPageModel).pipe();
   }
 
 }

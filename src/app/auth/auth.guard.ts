32d2260fdde7cb3f492ea1dcfa36ba95926b006f
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-03 14:27:53 +0100

    Fix Auth guard with NGRX store and removed authService.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,13 +9,16 @@ import {
   UrlSegment,
   UrlTree
 } from '@angular/router';
-import {Observable} from 'rxjs';
+import {map, Observable, take} from 'rxjs';
 import {AuthService} from "./auth.service";
+import {Store} from "@ngrx/store";
+import * as AuthSelector from './store/auth.selector';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authSl: AuthService,
-              private router: Router) {
+              private router: Router,
+              private store: Store) {
   }
 
   canActivate(
@@ -23,18 +26,32 @@ export class AuthGuard implements CanActivate, CanLoad {
     state: RouterStateSnapshot): Observable<boolean | UrlTree>
     | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (this.authSl.isAuth()) {
-      return true;
-    }
-    return this.router.createUrlTree(['auth/login']);
+    return this.store.select(AuthSelector.selectAuthPageViewModel)
+      .pipe(
+        take(1),
+        map(value => {
+          if (value.isAuth) {
+            return true
+          } else {
+            return this.router.createUrlTree(['auth/login']);
+          }
+        }));
+
   }
 
   canLoad(route: Route, segments: UrlSegment[]):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authSl.isAuth()) {
-      return true;
-    }
-    return this.router.createUrlTree(['auth/login']);
+    return this.store.select(AuthSelector.selectAuthPageViewModel)
+      .pipe(
+        take(1),
+        map(value => {
+          if (value.isAuth) {
+            return true
+          } else {
+            return this.router.createUrlTree(['auth/login']);
+          }
+        }));
+
   }
 
 }

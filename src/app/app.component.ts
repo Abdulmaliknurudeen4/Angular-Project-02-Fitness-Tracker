f4b61e6352b769aaf55ad5f3b0b5712406f35e1c
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-03 14:24:39 +0100

    Set AutoLogin Code with Effects and Ngrx Store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from "./auth/auth.service";
+import {Store} from "@ngrx/store";
+import * as AuthAction from '../app/auth/store/auth.actions';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,17 @@ import {AuthService} from "./auth/auth.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {
+  constructor(private afAuth: AngularFireAuth,
+              private store: Store) {
   }
 
   ngOnInit(): void {
-    this.authService.initAuthListener();
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.store.dispatch(AuthAction.AUTH_SUCCESS());
+      } else {
+        this.store.dispatch(AuthAction.AUTH_LOGOUT());
+      }
+    })
   }
 }

a348d70a2f1dd8a6b127e0326525b7d7fd938dd3
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-03 14:28:02 +0100

    module update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,6 @@ import {SignupComponent} from "./signup/signup.component";
 import {LoginComponent} from "./login/login.component";
 import {RouterModule} from "@angular/router";
 import {SharedModule} from "../shared.module";
-import {AuthService} from "./auth.service";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {StoreModule} from "@ngrx/store";
 import * as AppState from './store/auth.reducer';
@@ -17,14 +16,14 @@ import {AuthEffects} from "./store/auth.effects";
   ],
   imports: [
     RouterModule.forChild([
-    {path: 'signup', component: SignupComponent},
-    {path: 'login', component: LoginComponent},]),
+      {path: 'signup', component: SignupComponent},
+      {path: 'login', component: LoginComponent},]),
     SharedModule,
     StoreModule.forFeature(AppState.authFeature),
     EffectsModule.forFeature([AuthEffects])
   ],
   exports: [RouterModule],
-  providers: [AuthService,
+  providers: [
     {provide: MAT_DIALOG_DATA, useValue: {}}]
 })
 export class AuthModule {

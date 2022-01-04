ca85559a781ab77373e15b88d430360434477af0
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-01-04 14:10:50 +0100

    training.component ongoingTraingState updates.

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,34 +1,25 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from "rxjs";
-import {TrainingService} from "./training.service";
+import {Component, OnInit} from '@angular/core';
+import {map, Observable} from "rxjs";
+import {Store} from "@ngrx/store";
+import * as TrainingSelector from './store/training.selector';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit, OnDestroy {
-  ongoingTraining = false;
-  private trainingSubscription: Subscription | undefined;
+export class TrainingComponent implements OnInit {
+  ongoingTraining$: Observable<boolean> = new Observable<boolean>();
 
-  constructor(private trainingService: TrainingService) {
+  constructor(private store: Store) {
 
   }
 
   ngOnInit() {
-    this.trainingSubscription = this.trainingService
-      .exerciseChanged.subscribe(exercise=>{
-        if(!!exercise){
-          this.ongoingTraining = true;
-        }else{
-          this.ongoingTraining = false;
-        }
-      });
+    this.ongoingTraining$ = this.store.select(TrainingSelector.selectTrainingViewPageModel)
+      .pipe(map(trainingState => trainingState.runningEx), map(runningExec => {
+        return !!runningExec;
+      }));
   }
 
-
-  ngOnDestroy(): void {
-    if(this.trainingSubscription)
-      this.trainingSubscription.unsubscribe();
-  }
 }
